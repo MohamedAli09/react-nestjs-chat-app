76dcbc782d18d341f4c1585f5eecf591eb0bc9c2
MohamedAli09 <mohamed.ali00870@gmail.com>
2025-01-19 04:30:13 +0200

    Add user creation functionality with Apollo Client integration

--- client/src/components/auth/SignUp.tsx
+++ client/src/components/auth/SignUp.tsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
 import { Link as MuiLink } from "@mui/material";
 import Auth from "./Auth";
+import { useCreateUser } from "../../hooks/useCreateUser";
 
 const SignUp = () => {
+  const [createuser] = useCreateUser();
   return (
-    <Auth submitlabel="Signup" onSubmit={async () => {}}>
+    <Auth
+      submitlabel="Signup"
+      onSubmit={async ({ email, password }) => {
+        await createuser({
+          variables: {
+            createUserInput: {
+              email,
+              password,
+            },
+          },
+        });
+      }}
+    >
       <Link to={"/login"} style={{ alignSelf: "center" }}>
         <MuiLink>Login</MuiLink>
       </Link>
