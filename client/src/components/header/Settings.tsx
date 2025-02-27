3f9aaf0ea6ebcb91622890a82d846a1abdaa0969
MohamedAli09 <mohamed.ali00870@gmail.com>
2025-02-27 23:24:52 +0200

    Refactor header component by extracting navigation and settings into separate components

---
+++ client/src/components/header/Settings.tsx
@@ -0,0 +1,56 @@
+import {
+  Avatar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import * as React from "react";
+
+const Settings = ({
+  anchorElUser,
+  handleOpenUserMenu,
+  handleCloseUserMenu,
+  settings,
+}: {
+  anchorElUser: HTMLElement | null;
+  handleOpenUserMenu: (event: React.MouseEvent<HTMLElement>) => void;
+  handleCloseUserMenu: () => void;
+  settings: string[];
+}) => {
+  return (
+    <Box sx={{ flexGrow: 0 }}>
+      <Tooltip title="Open settings">
+        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+          <Avatar alt="Remy Sharp" src="" />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        sx={{ mt: "45px" }}
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        {settings.map((setting) => (
+          <MenuItem key={setting} onClick={handleCloseUserMenu}>
+            <Typography sx={{ textAlign: "center" }}>{setting}</Typography>
+          </MenuItem>
+        ))}
+      </Menu>
+    </Box>
+  );
+};
+
+export default Settings;
