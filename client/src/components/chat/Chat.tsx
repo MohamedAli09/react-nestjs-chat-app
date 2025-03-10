ee5d11881325fbd9fa611072fa8f974245b1ab67
MohamedAli09 <mohamed.ali00870@gmail.com>
2025-03-10 02:01:31 +0200

    feat: Enhance chat view layout and add message input component

--- client/src/components/chat/Chat.tsx
+++ client/src/components/chat/Chat.tsx
@@ -1,14 +1,41 @@
 import { useParams } from "react-router-dom";
 import useGetChat from "../../hooks/useGetChat";
+import { Stack, Paper, InputBase, IconButton, Divider } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 const Chat = () => {
   const params = useParams();
   const { data } = useGetChat({ _id: params._id! });
 
   return (
-    <div>
+    <Stack sx={{ height: "100%", justifyContent: "space-between" }}>
       <h1>{data?.chat.name}</h1>
-    </div>
+
+      <Paper
+        sx={{
+          p: "2px 4px",
+
+          display: "flex",
+
+          justifySelf: "flex-end",
+
+          alignItems: "center",
+
+          width: "100%",
+        }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1, width: "100%" }}
+          placeholder="Message"
+        />
+
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+
+        <IconButton color="primary" sx={{ p: "10px" }}>
+          <SendIcon />
+        </IconButton>
+      </Paper>
+    </Stack>
   );
 };
 
